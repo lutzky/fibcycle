d73e8e60f37c625a5c6c3269b338acd1963e3d42
Grazyna Skiba <grazyna.j.skiba@gmail.com>
2015-10-04 21:26:25 +0000

    Look for all cycles

--- main/fibcycle.go
+++ main/fibcycle.go
@@ -6,9 +6,12 @@ import (
 	fc "github.com/lutzky/fibcycle"
 )
 
-func analyzeFibLoop(initial fc.FibState) {
+func analyzeFibLoop(initial fc.FibState, usedGlobally map[fc.FibState]bool) {
 	loopLength, used := initial.FindCycle()
 	fmt.Printf("%v loops in %d steps\n", initial, loopLength)
+	for k, v := range used {
+		usedGlobally[k] = v
+	}
 	for _, thru := range []fc.FibState{
 		fc.NewFibState([]uint{0, 1}, 10),
 		fc.NewFibState([]uint{0, 2}, 10),
@@ -21,12 +24,8 @@ func analyzeFibLoop(initial fc.FibState) {
 }
 
 func main() {
-	for _, state := range []fc.FibState{
-		fc.NewFibState([]uint{0, 0}, 10),
-		fc.NewFibState([]uint{0, 1}, 10),
-		fc.NewFibState([]uint{2, 0}, 10),
-		fc.NewFibState([]uint{1, 0}, 10),
-	} {
-		analyzeFibLoop(state)
+	used := map[fc.FibState]bool{}
+	for state := fc.NewFibState([]uint{0, 0}, 10); !state.Equals(&fc.FibState{}); state = fc.FirstUnused(used) {
+		analyzeFibLoop(state, used)
 	}
 }
